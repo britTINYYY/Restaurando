6fe945bec4783167fe12e4b1679c4143d79b4354
Brittiny Tran <brittiny.tran@gmail.com>
2016-07-26 09:30:34 -0700

    Yelp API
    incorporated the Yelp API into workspace

---
+++ Restaurando/Restaurando-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Restaurando-Bridging-Header.h
+//  Restaurando
+//
+//  Created by Brittiny Tran on 7/25/16.
+//  Copyright © 2016 Brittiny Tran. All rights reserved.
+//
+
+#import <YelpAPI/YelpAPI.h>
--- Restaurando/ViewController.swift
+++ Restaurando/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let client = YLPClient(consumerKey: "hC6Q3AMvXrmnaithktTNcg",
+                               consumerSecret: "Vn8UMrw1TWWKfZyQXYAc6JX8rF4",
+                               token: "8i2e6oy5rnWhmfuovXsGHzR7GxBSrcO4",
+                               tokenSecret: "H50brSTiENoDplboaAgNfOvpmoM")
+        
+        client.searchWithLocation("San Francisco, CA",
+                                  currentLatLong: nil,
+                                  term: RestaurantTypes(type: "").getTypeRest(),
+                                  limit: 20,
+                                  offset: 15,
+                                  sort: .HighestRated) {
+        (search: YLPSearch?, error: NSError?) in
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
