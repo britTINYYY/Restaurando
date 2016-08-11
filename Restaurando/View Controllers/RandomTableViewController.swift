adfa2e9f50484f12bf9c5acf47148e5422c3fa8c
Brittiny Tran <brittiny.tran@gmail.com>
2016-08-10 17:18:45 -0700

    created Results Table View class

---
+++ Restaurando/View Controllers/RandomTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RandomTableViewController.swift
+//  Restaurando
+//
+//  Created by Brittiny Tran on 8/10/16.
+//  Copyright © 2016 Brittiny Tran. All rights reserved.
+//
+
+import Foundation
+
+class RandomTableViewController
+{
+    
+}
